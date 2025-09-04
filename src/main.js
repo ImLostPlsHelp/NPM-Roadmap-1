9d340211db28531bae2eb60a51144f190d677c01
ImLostPlsHelp <maurits1005@gmail.com>
2025-09-04 13:01:48 +0700

    Added timezone display functionality

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import './style.css'
 // import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter.js'
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc.js'
+import timezone from 'dayjs/plugin/timezone.js'
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 document.querySelector('#app').innerHTML = `
   <div class="date-container">
+  <p>${dayjs.tz.guess()}</p>
     <h1>${dayjs().format("HH:mm:ss")}</h1>
     <p>${dayjs().format("DD MMMM YYYY")}</p>
   </div>
